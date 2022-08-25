d4603049996476b75c616092594eb1bbb0822411
kmDuan <kmduan@archform.co>
2022-08-25 13:08:25 +0800

    1st commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the first React app hosted on aws-amplify by ArchForm
         </p>
         <a
           className="App-link"
